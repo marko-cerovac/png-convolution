f1d1e37173069da0673189b6f72b790a330c1603
marko-cerovac <marko.cerovac16@gmail.com>
2025-03-06 02:25:20 +0100

    confined Kernel template to type double

--- src/Kernel.h
+++ src/Kernel.h
@@ -2,16 +2,19 @@
 #include <array>
 #include <cstddef>
 #include <initializer_list>
-#include <stdexcept>
+#include <string>
+
+#include "utils/utils.h"
+#include "utils/exceptions.h"
 
 namespace ar {
-    template <typename T, std::size_t N>
+    template <size_t N>
     class Kernel {
       public:
 
         static_assert(N != 0, "Kernel dimension must be a non-zero value.");
 
-        Kernel() = delete;
+        Kernel() = default;
 
         Kernel(const std::initializer_list<double>& list) {
             unsigned int i = 0;
@@ -20,21 +23,17 @@ namespace ar {
             }
         }
 
-        T operator()(std::size_t n, std::size_t m) const {
+        double operator()(size_t n, size_t m) const { return matrix[n * N + m]; }
+
+        double& operator()(size_t n, size_t m) { return matrix[n * N + m]; }
+
+        template <size_t n, size_t m>
+        constexpr double at() const {
             return matrix[n * N + m];
         }
 
-        T& operator()(std::size_t n, std::size_t m) {
-            return matrix[n * N + m];
-        }
-
-        template <std::size_t n, std::size_t m>
-        constexpr T at() const {
-            return matrix[n * N + m];
-        }
-
-        template <std::size_t n, std::size_t m>
-        constexpr T& at() {
+        template <size_t n, size_t m>
+        constexpr double& at() {
             return matrix[n * N + m];
         }
 
@@ -46,10 +45,32 @@ namespace ar {
         constexpr inline auto end() { return matrix.end(); }
         constexpr inline auto cend() const { return matrix.cend(); }
 
-        /* static Kernel<T, N> parse(const std::string& input); */
+        static Kernel<N> parse(const std::string& input) {
+            auto params = utils::split(input);
+
+            if (params.size() != N * N) {
+                throw utils::ParseErrorException(
+                    "Parsing error: number of parameters doesn't match kernel dimentions");
+            }
+
+            Kernel<N> result;
+
+            size_t i = 0;
+            for (const auto& param : params) {
+                try {
+                    double temp = std::stod(param);
+                    result.matrix[i++] = temp;
+                } catch (const std::exception& err) {
+                    throw utils::ParseErrorException(
+                        "Parsing error: could not resolve parameter to a double");
+                }
+            }
+
+            return result;
+        }
 
       private:
 
-        std::array<T, N * N> matrix;
+        std::array<double, N * N> matrix;
     };
 }  // namespace ar
