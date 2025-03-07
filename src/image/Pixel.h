f57ad334359ca2a2ba5de155271d488320789a59
marko-cerovac <marko.cerovac16@gmail.com>
2025-03-07 18:57:07 +0100

    changed the order in the pixel struct to red->green->blue

--- src/image/Pixel.h
+++ src/image/Pixel.h
@@ -4,7 +4,7 @@
 #include <sys/types.h>
 
 struct Pixel {
-    u_int8_t blue;     // Blue component
-    u_int8_t green;    // Green component
     u_int8_t red;      // Red component
+    u_int8_t green;    // Green component
+    u_int8_t blue;     // Blue component
 };
