4e9a1e614e11b77b0beff0d033bab8759ebd76ad
marko-cerovac <marko.cerovac16@gmail.com>
2025-03-09 00:26:15 +0100

    reversed the order of pixel colors and added operators

--- src/image/Pixel.cpp
+++ src/image/Pixel.cpp
@@ -3,17 +3,55 @@
 namespace image {
     PixelHex::operator PixelNorm() const {
         return {
-            static_cast<float>(red) / 0xFF,
             static_cast<float>(blue) / 0xFF,
             static_cast<float>(green) / 0xFF,
+            static_cast<float>(red) / 0xFF,
         };
     }
 
     PixelNorm::operator PixelHex() const {
         return {
-            static_cast<uint8_t>(red * 0xFF),
             static_cast<uint8_t>(blue * 0xFF),
             static_cast<uint8_t>(green * 0xFF),
+            static_cast<uint8_t>(red * 0xFF),
+        };
+    }
+
+    PixelNorm PixelNorm::operator+(PixelNorm other) const {
+        return {blue + other.blue, green + other.green, red + other.red};
+    }
+
+    PixelNorm& PixelNorm::operator*=(float scalar) {
+        blue *= scalar;
+        green *= scalar;
+        red *= scalar;
+
+        return *this;
+    }
+
+    PixelNorm& PixelNorm::operator+=(PixelNorm other) {
+        blue += other.blue;
+        green += other.green;
+        red += other.red;
+
+        return *this;
+
+    }
+
+    PixelNorm operator*(float lhs, const PixelNorm& rhs) {
+        return {
+            rhs.blue * lhs,
+            rhs.green * lhs,
+            rhs.red * lhs,
+        };
+
+    }
+
+    PixelNorm operator*(const PixelNorm& lhs, float rhs) {
+        return {
+            lhs.blue * rhs,
+            lhs.green * rhs,
+            lhs.red * rhs,
         };
     }
 }  // namespace image
--- src/image/Pixel.h
+++ src/image/Pixel.h
@@ -8,19 +8,26 @@ namespace image {
 
     // pixel that uses hex values 0 - 0xFF
     struct PixelHex {
-        uint8_t red;      // Red component
-        uint8_t green;    // Green component
-        uint8_t blue;     // Blue component
+        uint8_t blue;   // Blue component
+        uint8_t green;  // Green component
+        uint8_t red;    // Red component
 
-        operator PixelNorm () const;
+        operator PixelNorm() const;
     };
 
     // normalized pixel that uses values 0.0 - 1.0
     struct PixelNorm {
-        float red;
-        float green;
-        float blue;
+        float blue;   // Blue component
+        float green;  // Green component
+        float red;    // Red component
 
-        operator PixelHex () const;
+        PixelNorm operator+(PixelNorm other) const;
+        PixelNorm& operator*=(float scalar);
+        PixelNorm& operator+=(PixelNorm other);
+        operator PixelHex() const;
     };
-}
+
+    PixelNorm operator*(float lhs, const PixelNorm& rhs);
+    PixelNorm operator*(const PixelNorm& lhs, float rhs);
+
+}  // namespace image
